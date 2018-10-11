47dbaac36757f5415733b4fb93e004c6dbdd86dd
ChaytonCallaghan <chaytoncallaghan@gmail.com>
2018-10-11 13:43:56 -0700

    FLCMT

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CoinFlip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int coin;
+          string userGuess;
+          string coinDescription = "Tails";
+          Random rng = new Random();
+
+          Console.Write("Enter your guess heads or Tails (H or T)");  
+          userGuess = Console.ReadLine();
+
+          coin = rng.Next(0, 2);
+          if (coin == 1)
+        {
+            coinDescription = "Heads";
+        }
+
+        if ((coin == 0 && userGuess == "T") || (coin == 1 && userGuess == "H"))
+        {
+            Console.WriteLine("The coin flip was {0}, you win!", coinDescription);
+        }
+        else
+        {
+            Console.WriteLine("The coin flip was {0}, you win!", coinDescription);
+        }
+
+        Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CoinFlip
+
+
+MDREAD.ME
+
+
+Student ID: ChaytonCallaghan or ChaytonC1
+
+Date: Oct,03,2018
+Date Completed: Oct,03,2018
